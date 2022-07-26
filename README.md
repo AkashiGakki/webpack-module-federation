932e13a9b2683c197a948342be32faa6dd1a5499
Akashi <akashi_sai@163.com>
2022-07-26 23:06:20 +0800

    :sparkles: feat: webpack module federation

---
+++ README.md
@@ -0,0 +1,7 @@
+# webpack-module-federation
+
+🧩 Webpack Module Federation Monorepo
+
+## License
+
+[MIT](./LICENSE) License © 2022 [Akashi Sai](https://github.com/akashigakki)
---
+++ packages/app1/src/index.js
@@ -0,0 +1,7 @@
+window.app2Url = "http://localhost:3002"
+
+import App from "./App"
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(<App />, document.getElementById("root"))
